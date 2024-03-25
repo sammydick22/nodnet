33c5878aca8756e1da4767f0b3fc8e10b9e3d609
Auger0 <kevin.taylor1924@gmail.com>
2024-03-25 00:27:51 -0700

    Restructured final code

---
+++ src/GUI.java
@@ -0,0 +1,111 @@
+package src;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    private boolean started = false;
+    private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel();
+    private JButton startStopButton = new JButton("Start");
+    private JButton resetButton = new JButton("Restore Defaults");
+    private JLabel statusLabel = new JLabel("Status: Stopped");
+
+    // Labels and TextFields for inputs
+    private JLabel sensLabel = new JLabel("Sensitivity:");
+    private JTextField sensBox = new JTextField("3", 2);
+    private JLabel threshLabel = new JLabel("Threshold:");
+    private JTextField threshBox = new JTextField("50", 2);
+    private JLabel accelLabel = new JLabel("Acceleration:");
+    private JTextField accelBox = new JTextField("10", 2);
+    private JLabel ipLabel = new JLabel("IP Address:");
+    private JTextField ipBox = new JTextField(10);
+    private JLabel portLabel = new JLabel("Port:");
+    private JTextField portBox = new JTextField("4242", 6);
+
+    // Default values
+    private final String DEFAULT_SENS = "3";
+    private final String DEFAULT_THRESH = "50";
+    private final String DEFAULT_ACCEL = "10";
+    private final String DEFAULT_IP = ""; // Assuming empty as default
+    private final String DEFAULT_PORT = "4242";
+
+    public String sens = "3";
+    public String thresh = "50";
+    public String accel = "10";
+    public String ip = ""; // Assuming empty as default
+    public String port = "4242";
+
+    public GUI() {
+        startStopButton.addActionListener(this);
+        resetButton.addActionListener(this);
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        panel.add(startStopButton, gbc);
+        panel.add(statusLabel, gbc);
+        addLabeledField(sensLabel, sensBox, panel, gbc);
+        addLabeledField(threshLabel, threshBox, panel, gbc);
+        addLabeledField(accelLabel, accelBox, panel, gbc);
+        addLabeledField(ipLabel, ipBox, panel, gbc);
+        addLabeledField(portLabel, portBox, panel, gbc);
+        panel.add(resetButton, gbc);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("GUI");
+        frame.pack();
+        frame.setVisible(true);
+        
+        
+    }
+
+    public int getSens() {
+        return Integer.parseInt(sensBox.getText());
+    }
+    public int getThresh() {
+        return Integer.parseInt(threshBox.getText());
+    }
+    public int getAccel() {
+        return Integer.parseInt(accelBox.getText());
+    }
+    public String getIP() {
+        return ipBox.getText();
+    }
+    public int getPort() {
+        return Integer.parseInt(portBox.getText());
+    }
+    public boolean getStarted() {
+        return started;
+    }
+
+    private void addLabeledField(JLabel label, JTextField field, JPanel panel, GridBagConstraints gbc) {
+        panel.add(label, gbc);
+        panel.add(field, gbc);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == startStopButton) {
+            started = !started;
+            startStopButton.setText(started ? "Stop" : "Start");
+            statusLabel.setText("Status: " + (started ? "Started" : "Stopped"));
+        } else if (e.getSource() == resetButton) {
+            // Resetting values to default
+            sensBox.setText(DEFAULT_SENS);
+            threshBox.setText(DEFAULT_THRESH);
+            accelBox.setText(DEFAULT_ACCEL);
+            ipBox.setText(DEFAULT_IP);
+            portBox.setText(DEFAULT_PORT);
+        }
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
---
+++ src/NodNet.java
@@ -0,0 +1,180 @@
+package src;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.awt.event.KeyEvent;
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+
+public class NodNet implements NativeKeyListener{
+    private static int screenWidth;
+    private static int screenHeight;
+    private static double pitchScaler;
+    private static double sensitivity;
+    private static GUI gui = new GUI();
+    private static double acceleration;
+    private static String ipv4;
+    private static int port;
+    private static boolean altDown = false;
+    private static boolean xDown = false;
+    private static boolean cDown = false;
+    private static boolean zDown = false;
+    private static boolean isLeftPressing = false;
+    private static boolean isRightPressing = false;
+    private static boolean locked = false;
+
+    public static void main(String[] args) {
+        try {
+            initializeScreenDimensions();
+            
+            System.out.println(sensitivity);
+            System.err.println(acceleration);
+            GlobalScreen.registerNativeHook();
+            GlobalScreen.addNativeKeyListener(new NodNet());
+            java.util.logging.Logger logger = java.util.logging.Logger.getLogger(GlobalScreen.class.getPackage().getName());
+            logger.setLevel(java.util.logging.Level.OFF);
+            new Thread(() -> {
+                try {
+                    Robot robot = new Robot();
+                    while (true) {
+                        if(altDown && zDown) locked = !locked;
+                        if(!locked){
+                            if (altDown && cDown) {
+                                if(!isRightPressing){
+                                    isRightPressing = true;
+                                    robot.mousePress(KeyEvent.BUTTON3_DOWN_MASK);
+                                }
+                            }
+                            else{
+                                if(isRightPressing){
+                                    isRightPressing = false;
+                                    robot.mouseRelease(KeyEvent.BUTTON3_DOWN_MASK);
+                                }
+                                if (altDown && xDown) {
+                                    if(!isLeftPressing){
+                                        isLeftPressing = true;
+                                        robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+                                    }
+                                }
+                                else{
+                                    if(isLeftPressing){
+                                        isLeftPressing = false;
+                                        robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
+                                    }
+                                }
+                            }
+                        }
+                        robot.delay(100); // Delay to avoid high CPU usage
+                    }
+                } catch (AWTException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            while(true){
+                System.out.println(locked);
+                if(gui.getStarted() && !locked){
+                    sensitivity = gui.getSens()/10.0;
+                    acceleration = 1 + gui.getAccel()/10.0;
+                    ipv4 = gui.getIP();
+                    port = gui.getPort();
+                    receiveFreelookData(ipv4, port); // IP and port can be adjusted
+                }
+            } 
+        } catch (NativeHookException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void initializeScreenDimensions() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenWidth = screenSize.width;
+        screenHeight = screenSize.height;
+        pitchScaler = screenWidth/screenHeight;
+    }
+
+    private static void receiveFreelookData(String ip, int port) {
+        try (DatagramSocket socket = new DatagramSocket(port)) {
+            System.out.println("Listening for freelook data on IP " + ip + " : " + port);
+            byte[] buffer = new byte[48]; // Buffer for incoming data
+
+            while (gui.getStarted()) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                long timeReceived = System.currentTimeMillis();
+                socket.receive(packet);
+                processFreelookData(packet.getData(), timeReceived);
+            }
+        } catch (SocketException e) {
+            System.err.println("Socket exception: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static void processFreelookData(byte[] data, long timeReceived) {
+        if (data.length != 48) {
+            System.out.println("Received data of unexpected length.");
+            return;
+        }
+
+        ByteBuffer buffer = ByteBuffer.wrap(data);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        double yaw = buffer.getDouble(3 * Double.BYTES);
+        double pitch = -buffer.getDouble(4 * Double.BYTES); // Inverting pitch
+        
+        
+        int negOrNotY = 1;
+        int negOrNotP = 1;
+        if (yaw < 0) {
+            negOrNotY = -1;
+        }
+        if (pitch < 0) {
+            negOrNotP = -1;
+        }
+        
+        moveMouse(negOrNotY*Math.abs(Math.pow(yaw*sensitivity, acceleration))/acceleration, negOrNotP*Math.abs(Math.pow(pitch*sensitivity, acceleration))*pitchScaler/acceleration);
+        long timeBeforeMove = System.currentTimeMillis();
+        long delay = timeBeforeMove - timeReceived;
+        System.out.println(yaw + " " + pitch + " " + "delay: " + delay + "ms" + "locked: "+locked);
+    }
+
+    private static void moveMouse(double yaw, double pitch) {
+        try {
+            if(locked) return;
+            Robot robot = new Robot();
+            Point mousePoint = MouseInfo.getPointerInfo().getLocation();
+            int newX = (int) (mousePoint.x + yaw);
+            int newY = (int) (mousePoint.y + pitch);
+
+            robot.mouseMove(newX, newY);
+        } catch (AWTException e) {
+            System.err.println("AWTException: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        if (e.getKeyCode() == NativeKeyEvent.VC_ALT) altDown = true;
+        else if (e.getKeyCode() == NativeKeyEvent.VC_X) xDown = true;
+        else if (e.getKeyCode() == NativeKeyEvent.VC_C) cDown = true;
+        else if (e.getKeyCode() == NativeKeyEvent.VC_Z) zDown = true;
+    }
+
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        if (e.getKeyCode() == NativeKeyEvent.VC_ALT) altDown = false;
+        else if (e.getKeyCode() == NativeKeyEvent.VC_X) xDown = false;
+        else if (e.getKeyCode() == NativeKeyEvent.VC_C) cDown = false;
+        else if (e.getKeyCode() == NativeKeyEvent.VC_Z) zDown = false;
+    }
+}
